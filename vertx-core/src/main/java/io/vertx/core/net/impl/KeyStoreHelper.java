a71a366447feb63bcc8d6805cbb1fa0546d4cad7
Dimitri Fankhauser <135702874+DimitriFankhauser@users.noreply.github.com>
2026-05-13 15:36:24 +0200

    feat: modified PKCS11-keystores for HSM-based TLS in Quarkus

--- vertx-core/src/main/java/io/vertx/core/net/impl/KeyStoreHelper.java
+++ vertx-core/src/main/java/io/vertx/core/net/impl/KeyStoreHelper.java
@@ -112,6 +112,10 @@ public class KeyStoreHelper {
             // It's a private key
             continue;
           }
+          if (key.getFormat() == null) {
+            // Hardware security module key, not extractable
+            continue;
+          }
           X509KeyManager mgr = new X509KeyManager() {
             @Override
             public String[] getClientAliases(String s, Principal[] principals) {
@@ -254,10 +258,12 @@ public class KeyStoreHelper {
       if (!ks.containsAlias(alias)) {
         throw new IllegalArgumentException("alias does not exist in the keystore: " + alias);
       }
-      List<String> ksAliases = Collections.list(ks.aliases());
-      for (String ksAlias : ksAliases) {
-        if (!alias.equals(ksAlias)) {
-          ks.deleteEntry(ksAlias);
+      if (!type.equalsIgnoreCase("PKCS11")) {
+        List<String> ksAliases = Collections.list(ks.aliases());
+        for (String ksAlias : ksAliases) {
+          if (!alias.equals(ksAlias)) {
+            ks.deleteEntry(ksAlias);
+          }
         }
       }
     }
